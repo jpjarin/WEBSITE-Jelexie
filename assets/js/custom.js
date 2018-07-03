009d003d4501f08a2717e2fc40df85fb8019d095
JP <37213356+jpjarin@users.noreply.github.com>
2018-07-03 17:07:25 +0800

    new commit

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,18 @@
 new WOW().init();
 
+
+//Preloader
+$(window).on('DOMContentLoaded', function() {
+	    var Body = $('body,html');
+	    Body.css({"overflow":"hidden"});
+	});
+	$(window).on('load', function() {
+	    $('.preloader').fadeOut();
+	    $('body,html').css({"overflow":"visible"});
+	});
+
+
+
 $(window).on('load', function() {
 	    $('.about-heading,.about-description,.read-more').css({"opacity":"100"});
 	    $('.about-heading,.about-description').addClass('animated fadeIn');
@@ -11,39 +24,15 @@ $(window).on('load', function() {
     $("#navbar").sticky({topSpacing:0,zIndex:999});
   });
 
- jQuery.fn.extend({
-	rwd: function(){
-		var fb = this;
-		var id = 'fb-box-container-' + Math.floor(Math.random() * 10000);
-		$(fb).wrap('<div id="' + id + '" style="width: 100%; height: 100%;">');
-		
-		$(window).resize(function(){
-			var r_w = 1;
-			//clear transform
-			$(fb).attr('style', '');
-			
-			var win_w = $('#' + id).width();
-			
-			//default width height 500px
-			var fb_w = parseInt($(fb).attr('data-width')) || 500;
-			var fb_h = fb_w;
-			r_w = win_w/fb_w;
-			
-			$(fb)
-				.css('left', ((fb_w*r_w-fb_w)/2) + 'px')
-				.css('top',  ((fb_h*r_w-fb_h)/2) + 'px')
-				.css('transform', 'scale('+r_w+', '+r_w+')');
-				
-		}).resize();
-	}
-});
 
-//Ads
-$('#ads').carousel({
-  interval: 6000
-})
+ $(document).ready(function(){
+   //Ads
+	$('#ads').carousel({
+	  interval: 6000
+	});
 
-//Carousel
-$('#carousel').carousel({
-  interval: 500000
-})
+	//Carousel
+	$('#carousel').carousel({
+	  interval: 5000
+	});
+  });
--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	<link rel="icon" href="favicon.png">
+	<link rel="stylesheet" type="text/css" href="assets/css/pre-loader.css">
 	<link href="assets/css/style.css" rel="stylesheet" type="text/css" />
 	<link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
 	<link rel="stylesheet" type="text/css" href="assets/fonts/fontawesome/css/all.css" media="all">
@@ -23,6 +24,14 @@
   js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0';
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
+
+
+<div class="preloader">
+		<div class="load">
+			<div class="loader"><div class="jelexie-loading"><img src="assets/img/logo-min.png" style="width: 33vw;height: 100%;"></div><div class="loading"><p class="loading"><!-- Loading --><span>.</span><span>.</span><span>.</span></p></h1></div>
+			</div>
+		</div>
+	</div>
 
 <div id="main" class="main">
 <?php include 'assets/includes/navbar.html';?>
@@ -59,10 +68,10 @@
 		<div class="row">
 			<div class="col-md-8">
 				<div class="about-content">
-				<h1 class="about-heading mt-0 pt-2">ABOUT US</h1>
-				<p class="about-description" style="animation-delay: 0.2s;">
+				<h1 class="about-heading mt-0 pt-2" style="animation-delay: 0.2s;">ABOUT US</h1>
+				<p class="about-description" style="animation-delay: 0.3s;">
 					Behind our company’s success are different people united by the same vision — one summarized in ensuring total customer satisfaction.
-					<div class="hvr-icon-pulse-grow read-more float-right" style="animation-delay: 0.8s;"><a href="#" class="hvr-float-shadow">Read more <i class="hvr-icon fa fa-angle-double-right" style="position: relative; top: 0.5px;"></i></a></div>
+					<div class="hvr-icon-pulse-grow read-more float-right" style="animation-delay: 0.8s;"><a href="index.html" class="hvr-float-shadow">Read more <i class="hvr-icon fa fa-angle-double-right" style="position: relative; top: 0.5px;"></i></a></div>
 				</p>
 			</div>
 			</div>
@@ -79,37 +88,38 @@
 		<div class="container-fluid pl-2 mt-2">
 			<div class="row">
 				<div class="col-sm-6 mb-4">
-					<div id="carousel" class="h-100 carousel slide carousel-fade" data-ride="carousel">
+					<div id="carousel" class="wow slideInUp h-100 carousel slide carousel-fade" data-ride="carousel">
 					<ol class="carousel-indicators">
 					    <li data-target="#carousel" data-slide-to="0" class="active"></li>
 					    <li data-target="#carousel" data-slide-to="1"></li>
 					</ol>
-					  <div class="carousel-inner">
-					    <div class="carousel-item h-100 active">
-					       <h1 class="text-center mt-3 mb-2">WE ARE HIRING!</h1>
-					       <p class="text-center px-4 pt-1 pb-3">
+					  <div class="carousel-inner employees">
+					    <div class="carousel-item active">
+					       <h1 class="text-center mt-4 mb-2">WE ARE HIRING!</h1>
+					       <p class="text-center px-4 pt-1 pb-2">
 					       	MANAGEMENT ENGINEER, ACCOUNTING OFFICER, WAREHOUSE OFFICER, PROGRAMMER, HARDWARE SPECIALIST, MARKETING STAFF, AUDIT STAFF, MANAGEMENT TRAINEE, STORE CREW, PRODUCTION CREW, SAP SYSTEM ANALYST / ADMINISTRATOR, POS SYSTEM ADMINISTRATOR
 					       </p>
-					       <div class="mx-auto text-center">
+					       <div class="mx-auto text-center pb-4">
 					       		<a class="btn-custom" href="">APPLY NOW</a>
 					   		</div>
 					    </div>
-					    <div class="carousel-item h-100">
+					    <div class="carousel-item">
 					      <h1 class="text-center pb-2 px-4 mt-5">BE PART OF OUR JELEXIE BRANCHES NOW!</h1>
 					       <p class="text-center pb-4 px-3">
 					       	Please Contact Us For More Info
 					       </p>
-					       <div class="mx-auto text-center">
-					       		<a class="btn-custom pb-2" href="">CONTACT US</a>
+					       <div class="mx-auto text-center pb-4">
+					       		<a class="btn-custom" href="">CONTACT US</a>
 					   		</div>
 					    </div>
 					  </div>
-					  <img src="assets/img/employees.png" class="img-fluid align-bottom employees pt-1">
+					  <!-- <img src="assets/img/employees.png" class="img-fluid align-bottom employees pt-5"> -->
 					</div>
 				</div>
 
-				<div class="col-sm-6 px-2 mb-4">
-					<div class="fb-page" data-href="https://facebook.com/Jelexie-179456868470/" data-tabs="timeline, events, messages" data-width="500" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://facebook.com/Jelexie-179456868470/" class="fb-xfbml-parse-ignore"><a href="https://facebook.com/Jelexie-179456868470/">Jelexie</a></blockquote></div>
+				<div class="col-sm-6 px-2 mb-4"><div class="container-fluid mx-auto wow slideInUp" data-wow-delay="0.1s">
+					<div class="fb-page" data-href="https://facebook.com/Jelexie-179456868470/" data-tabs="timeline, messages" data-width="500" data-small-header="true" data-adapt-container-width="true" data-hide-cta="false" data-height="565" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://facebook.com/Jelexie-179456868470/" class="fb-xfbml-parse-ignore"><a href="https://facebook.com/Jelexie-179456868470/">Jelexie</a></blockquote></div>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -118,12 +128,11 @@
 <?php include 'assets/includes/footer.html';?>
 </div>
 
-
+<script src="assets/js/pace.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
 <script src="assets/js/wow.min.js"></script>
   <script src="assets/js/bootstrap.min.js"></script>
-  <script src="assets/js/jquery.fittext.js"></script>
   <script src="assets/js/jquery.sticky.js"></script>
   <script src="assets/js/custom.js"></script>
 </body>
